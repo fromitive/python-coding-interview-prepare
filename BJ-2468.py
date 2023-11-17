30cc3422be3db1e96c6409b0b52de398aac5f57f
fromitive <fromitive@gmail.com>
2023-11-17 17:57:00 +0900

    feat: 2468 문제 풀이 수정(예외 케이스)

--- BJ-2468.py
+++ BJ-2468.py
@@ -1,5 +1,8 @@
-MINIMUM_HEIGHT = 1
+import sys
+from collections import deque
 
+MINIMUM_HEIGHT = 0
+input = sys.stdin.readline
 # //-- debug functions--//
 
 
@@ -32,48 +35,37 @@ def setup_input() -> (int, int, list):
 
 def solve(N, max_height, map_list):
     max_safe_zone = 0
-    for height in range(1, max_height + 1):
-        visit_map = make_visit_map(N, height, map_list)
-        safe_zone = search_safe_zone(N, visit_map)
+    for height in range(0, max_height + 2):
+        safe_zone = search_safe_zone(N, height, map_list)
         max_safe_zone = max(safe_zone, max_safe_zone)
 
     print(max_safe_zone)
 
 
-def make_visit_map(N, height, map_list):
-    # 1 can visit
-    # 0 can't visit
+def search_safe_zone(N, height, map_list):
+    safe_zone = 0
     visit_map = [[1] * N for _ in range(N)]
     for xpos in range(N):
         for ypos in range(N):
-            if map_list[xpos][ypos] <= height:
-                visit_map[xpos][ypos] = 0
-    return visit_map
-
-
-def search_safe_zone(N, visit_map):
-    safe_zone = 0
-
-    for xpos in range(N):
-        for ypos in range(N):
             start_pos = [xpos, ypos]
-            to_visit = [start_pos]
-            if visit_map[xpos][ypos] == 1:
-                search_map(to_visit, visit_map)
+            to_visit = deque()
+            to_visit.append(start_pos)
+            if map_list[xpos][ypos] > height and visit_map[xpos][ypos] == 1:
+                search_map(to_visit, visit_map, map_list, height, N)
                 safe_zone += 1
-    print("finished : ", safe_zone)
+    # print("height: ", height, "finished : ", safe_zone)
     return safe_zone
 
 
-def search_map(to_visit, visit_map):
+def search_map(to_visit, visit_map, map_list, height, N):
     while to_visit:
-        debug_visit_map(visit_map, to_visit)
-        xpos, ypos = to_visit.pop()
+        #debug_visit_map(visit_map, to_visit)
+        xpos, ypos = to_visit.popleft()
         visit_map[xpos][ypos] = 0
-        search_visitable(xpos, ypos, N, visit_map, to_visit)
+        search_visitable(xpos, ypos, N, visit_map, to_visit, map_list, height)
 
 
-def search_visitable(xpos, ypos, N, visit_map, to_visit):
+def search_visitable(xpos, ypos, N, visit_map, to_visit, map_list, height):
     direction = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     for dx, dy in direction:
         next_x = xpos + dx
@@ -83,6 +75,7 @@ def search_visitable(xpos, ypos, N, visit_map, to_visit):
             and 0 <= next_y < N
             and visit_map[next_x][next_y] == 1
             and [next_x, next_y] not in to_visit
+            and map_list[next_x][next_y] > height
         ):
             to_visit.append([next_x, next_y])
 
@@ -98,7 +91,4 @@ def debug_visit_map(visit_map, to_visit):
 
 if __name__ == "__main__":
     N, max_height, map_list = setup_input()
-
-    print_map(map_list)
-
     solve(N, max_height, map_list)
