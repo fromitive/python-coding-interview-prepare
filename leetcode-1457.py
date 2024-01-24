87c24501fe7f16c1732c2050e6fe949827183084
fromitive <fromitive@gmail.com>
2024-01-24 16:44:32 +0900

    feat: 다양한 방법 추가

--- leetcode-1457.py
+++ leetcode-1457.py
@@ -4,6 +4,8 @@ from typing import List
 from typing import Optional
 from collections import deque
 from collections import Counter
+from abc import abstractmethod
+from abc import ABCMeta
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -39,6 +41,73 @@ class TreeNode:
 
         return root
 
+class PathStorage(metaclass=ABCMeta):
+    @abstractmethod
+    def addPath(node: TreeNode):
+        pass
+    
+    @abstractmethod
+    def is_pesudo_palindromic() -> bool:
+        pass
+    
+    @abstractmethod
+    def show_storage() -> None:
+        pass
+    
+class PathByListStorage(PathStorage):
+    def __init__(self, storage:List[int] = []):
+        self.storage = storage
+    
+    def addPath(self,node: TreeNode):
+        return PathByListStorage(self.storage + [node.val])
+    
+    def is_pesudo_palindromic(self) -> bool:
+        odd_count = 0
+        for item in self.storage:
+            # count item and check if the number of item is odd
+            if self.storage.count(item) % 2 == 1:
+                odd_count +=1
+                if odd_count > 1:
+                    return False
+        return True
+    
+    def show_storage(self) -> None:
+        print('storage', self.storage)
+
+
+class PathByStringStorage(PathStorage):
+    def __init__(self, storage:str = ""):
+        self.storage = storage
+    
+    def addPath(self,node: TreeNode):
+        return PathByStringStorage(self.storage + str(node.val))
+    
+    def is_pesudo_palindromic(self) -> bool:
+        odd_count = 0
+        for item in range(1, 10):
+            # count item and check if the number of item is odd
+            if self.storage.count(str(item)) % 2 == 1:
+                odd_count +=1
+                if odd_count > 1:
+                    return False
+        return True
+    
+    def show_storage(self) -> None:
+        print('storage', self.storage)
+
+class PathByBitStorage(PathStorage):
+    def __init__(self, storage:int = 0):
+        self.storage = storage
+    
+    def addPath(self,node: TreeNode):
+        return PathByBitStorage(self.storage ^ (1 << node.val))
+    
+    def is_pesudo_palindromic(self) -> bool:
+        return self.storage & (self.storage - 1) == 0
+    
+    def show_storage(self) -> None:
+        print('storage', self.storage)
+
 class Solution:
     result = 0
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
@@ -72,14 +141,35 @@ class Solution:
             if node.right:
                 stack.append((node.right, bit_storage ^ (1 << node.right.val)))
         return answer
+    
+    def pseudoPalindromicPaths_using_PathStorage(self, root: Optional[TreeNode], path: Optional[PathStorage]) -> int:
+        stack = [(root, path.addPath(root))]
+        answer = 0
+        while stack:
+            node, path = stack.pop()
+            if node.left == None and node.right == None:
+                if path.is_pesudo_palindromic():
+                    answer += 1
+
+            if node.left:
+                stack.append((node.left, path.addPath(node.left)))
+            if node.right:
+                stack.append((node.right, path.addPath(node.right)))
+
+        return answer
+
 
 null = None
 test_cases = [
     ([2,3,1,3,1,None,1], 2),
     ([2,1,1,1,3,None,None,None,None,None,1], 1),
-    ([1,9,1,null,1,null,1,null,null,7,null,null,4],1)
+    ([1,9,1,null,1,null,1,null,null,7,null,null,4],1),
+    ([6,6,6,null,6,6,null,null,null,2,8,8,8,3,2,5,6,null,8,null,null,1,1,7,9,null,null,null,null,null,null,null,null,5,null,null,4],1)
 ]
 
+
 for test_case,expected in test_cases:
+    storage = PathByBitStorage()
     print(test_case, Solution().pseudoPalindromicPaths(TreeNode.of(test_case)), expected)
     print(test_case, Solution().pseudoPalindromicPaths_using_stack(TreeNode.of(test_case)), expected)
+    print(test_case, Solution().pseudoPalindromicPaths_using_PathStorage(TreeNode.of(test_case), storage), expected)
