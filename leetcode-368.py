1ba3d8efa6cd7b4d109c0f88bea5705896dff11e
fromitive <fromitive@gmail.com>
2024-02-09 15:30:24 +0900

    style: dynamic programming 설명 추가

--- leetcode-368.py
+++ leetcode-368.py
@@ -9,6 +9,9 @@ class Solution:
         for end_idx in range(len(nums)):
             for start_idx in range(end_idx):
                 if nums[end_idx] % nums[start_idx] == 0 and len(answers[end_idx]) < len(answers[start_idx]) + 1:
+                    # 조건을 만족하고, 원래 있던 answers[start_idx]보다 더 큰값이 있으면 값을 업데이트 해준다. 
+                    # 업데이트가 가능한 이유는 nums[end_idx] % nums[start_idx]를 만족하기 때문이다.
+                    # nums는 정렬되어 있는 상태라, 위의 조건만 만족하면 된다.
                     answers[end_idx] = answers[start_idx] + [nums[end_idx]]
         return max(answers, key=len)
 test_cases = [
